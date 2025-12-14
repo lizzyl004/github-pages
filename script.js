9b76297bd473e5e9b916f9074ceae29ddde36cef
lizzyl004 <lizzyl@ksu.edu>
2025-12-14 13:39:50 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function({
+document.addEventListener("DOMContentLoaded", function(){
   var buttons = document.getElementByClassName(".footerButton");
   buttons.forEach(function (button) {
     button.addEventListener("click", function() {
       var link = button.getAttribute("data-link")
       window.location.href = link;
     });
-  });
+  }
 });
